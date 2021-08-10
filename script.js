fa4d50ddd3fd8bb857bedcb3e5790e95926c2d16
Arthur Saldanha <30752069+arthursaldanha@users.noreply.github.com>
2021-08-10 18:42:54 -0300

    Consertos no Design e na Funcionalidade JS

--- script.js
+++ script.js
@@ -5,7 +5,8 @@
       let select = document.querySelector("#cryptos__option");
       let value = select.options[select.selectedIndex].value;
       let text = select.options[select.selectedIndex].text;
-      console.log(text);
+
+      console.log(text, value);
 
       if (value !== "") {
          showWarining("Carragando...")
@@ -13,23 +14,37 @@
          let URL = `https://www.mercadobitcoin.net/api/${value}/ticker/`;
          let res = await fetch(URL);
          let json = await res.json();
+         console.log(json);
+
+         json.ticker.last = parseFloat(json.ticker.last).toFixed(2);
+         json.ticker.open = parseFloat(json.ticker.open).toFixed(2);
 
          showInfo({
-            price: json.last,
-            high: json.high,
-            low: json.low
+            price: json.ticker.last,
+            open: json.ticker.open,
+            high: json.ticker.high,
+            low: json.ticker.low
          })
+         
       } else {
          showWarining("Selecione uma das opções corretamente!");
       }
+
+      function showInfo(json) {
+         showWarining("")
+   
+         document.querySelector(".result").style.display = "block"
+         document.querySelector(".name").innerHTML = text;
+         document.querySelector(".symbol").innerHTML = value;
+         document.querySelector(".price").innerHTML = `Cotação Atual: <b>R$ ${json.price}</b>`
+         document.querySelector(".variation").innerHTML = `Variação em 24hrs: ${(json.price / json.open)}`;
+         
+         let image = document.querySelector(".image")
+         image.src = `./Images/${value}.png`
+      }
    })
 
-   function showInfo(json) {
-      showWarining("")
-
-   }
-
    function showWarining(msg) {
-      document.querySelector(".aviso").textContent = msg;
+      document.querySelector(".warning").innerHTML = msg;
    };
 })();
